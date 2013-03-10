3e8aec8ef4820f979838681759deeda5090cb367
Soheil Jadidian <soheil@jadidian.com>
2013-03-10 11:09:23 -0400

    started working on skin.js, which is the main object and namespace for other stuff

--- source/scripts/skin.js
+++ source/scripts/skin.js
@@ -1,22 +1,42 @@
+// entry point
 (function() {
-  // configuring paths for loading libraries
-  // either jQuery or Zepto can be used
+
+  // configuring paths for loading other libraries
+  // jQuery, Zepto or other libraries can be used
+  // TODO: Underscore and Backbone might be needed only for data rich components
   require.config({
     paths: {
       '$': '../jquery',
       '_': '../underscore',
-      'Backbone': '../backbone'
+      'backbone': '../backbone'
     }
   });
 
-  // defining skin main module and namespace
-  // TODO: Underscore and Backbone might be needed only for data rich components
-  define('skin', ['$', '_', 'Backbone'], function() {
-    var that = this,
-        Skin = that.Skin = {};
-    Skin.$ = that.$;
+  // define Skin module when $ is loaded
+  require(['$'], function() {
+    // setting references
+    // also cache the old Skin if exists
+    // TODO: return old skin in noConflict mode
+    var that    = this,
+        oldSkin = that.Skin,
+        Skin    = that.Skin   = {},
+        $       = Skin.$      = that.$;
 
-    return Skin;
+    // extend the Skin object with default options and methods
+    $.extend(Skin, {
+      VERSION: '0.0.0',
+      defaults: {
+        // include the parser or not
+        // parser will scan HTML tags and
+        // binds events to create components
+        parse: true,
+      },
+      initialize: function() {
+        console.log(that);
+      }
+    });
+
+    Skin.initialize();
   });
 
 }).call(this);
