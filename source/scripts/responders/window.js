1618bc993de4b44f214b54e20bdddaca8c3d57a1
Soheil Jadidian <soheil@jadidian.com>
2013-04-26 20:10:56 -0400

    refactored string keys in Window responder

--- source/scripts/responders/window.js
+++ source/scripts/responders/window.js
@@ -5,12 +5,23 @@
 
 define('responders/window', ['skin'], function(Skin) {
 
+
   // Window Responder Module
   // =======================
   // hooks for window events
   // supports resize, scroll, load, unload, hashchange
 
-  var w = window, d = document, e = d.documentElement, b = d.body, Tools = Skin.Tools, hub = {}, width, height, x, y;
+
+  var w = window, d = document, e = d.documentElement, b = d.body, Tools = Skin.Tools, hub = {}, width, height, x, y
+
+    // string keys
+    , ON          = 'on'
+    , RESIZE      = 'resize'
+    , SCROLL      = 'scroll'
+    , LOAD        = 'load'
+    , UNLOAD      = 'unload'
+    , HASH_CHANGE = 'hashchange';
+
 
   function on(emitter, name, context) {
     // indices is not needed, because all events are window's
@@ -23,13 +34,14 @@ define('responders/window', ['skin'], function(Skin) {
     // also keep a count and reference for contexts listening,
     // so we can easily remove all bindings for a context
     // or remove the name from hub when there's no context listening
-    hub[name] = { contexts: [context] , old: w['on' + name] };
-    w['on' + name] = function(e) {
+    hub[name] = { contexts: [context] , old: w[ON + name] };
+    w[ON + name] = function(e) {
       handle(e);
       // calling the old handler
       if (Tools.isFunction(hub[name].old)) hub[name].old(e);
     };
   }
+
 
   function off(emitter, name, context) {
     // special case, when there's no name it means
@@ -40,7 +52,7 @@ define('responders/window', ['skin'], function(Skin) {
         // remove all context references from all handlers
         Tools.reject(handler.contexts, function(which) { return which === context; });
         if (!handler.contexts.length) {
-          w['on' + name] = handler.old;
+          w[ON + name] = handler.old;
           delete hub[name];
         }
       });
@@ -50,40 +62,43 @@ define('responders/window', ['skin'], function(Skin) {
     // remove one of the context references, it may have other namespaced listeners
     Tools.remove(hub[name].contexts, context);
     if (!hub[name].contexts.length) {
-      w['on' + name] = hub[name].old;
+      w[ON + name] = hub[name].old;
       delete hub[name];
     }
   }
 
+
   function handle(e) {
     switch (e.type) {
 
-      case 'resize':
+      case RESIZE:
         width  = w.innerWidth  || e && e.clientWidth  || 0;
         height = w.innerHeight || e && e.clientHeight || 0;
-        Skin.trigger(w, 'resize', { width: width, height: height, event: e });
+        Skin.trigger(w, RESIZE, { width: width, height: height, event: e });
       break;
 
-      case 'scroll':
+      case SCROLL:
         x = w.pageXOffset || e && e.scrollLeft || b && b.scrollLeft || 0;
         y = w.pageYOffset || e && e.scrollTop  || b && b.scrollTop  || 0;
-        Skin.trigger(w, 'scroll', { x: x, y: y, event: e });
+        Skin.trigger(w, SCROLL, { x: x, y: y, event: e });
       break;
 
-      case 'load':
-        Skin.trigger(w, 'load', { event: e });
+      case LOAD:
+        Skin.trigger(w, LOAD, { event: e });
       break;
 
-      case 'unload':
-        Skin.trigger(w, 'unload', { event: e });
+      case UNLOAD:
+        Skin.trigger(w, UNLOAD, { event: e });
       break;
 
-      case 'hashchange':
-        Skin.trigger(w, 'hashchange', { hash: w.location.hash, event: e });
+      case HASH_CHANGE:
+        Skin.trigger(w, HASH_CHANGE, { hash: w.location.hash, event: e });
       break;
 
     }
   }
+
+
 
 
   var Window = Tools.extend(Skin.Responders.Window, { on: on, off: off });
